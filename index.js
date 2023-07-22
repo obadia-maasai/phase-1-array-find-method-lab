444deefa99ec0d32ad12b28763c4cabea82a8520
obadia <obadia.maasai@student.moringaschool.com>
2023-07-22 14:16:56 +0300

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    
+    const winningYearObject = arrayOfObjects.find((obj) => obj.result === "W");
+  
+    
+    return winningYearObject ? winningYearObject.year : undefined;
+  }
+  const superBowlHistory = [
+  { year: 2019, result: "W" },
+  { year: 2012, result: "W" },
+  { year: 2023, result: "N/A" },
+  { year: 2021, result: "L" },
+  { year: 2018, result: "W" },
+];
+
+console.log(superbowlWin(superBowlHistory)); 
+  
